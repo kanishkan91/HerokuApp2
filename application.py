bc0b5ed23a08c02726c352b9a0610b8a31b734d7
Kanishka Narayan <37234086+kanishkan91@users.noreply.github.com>
2019-02-19 01:48:54 -0500

    Update application.py

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import numpy as np
-import technical_indicators as ts
+#import technical_indicators as ts
 import pandas as pd
 import pandas
 import xlsxwriter
